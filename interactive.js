9cdb6d1a02644c9027b9dc9a0b57d43b16470f87
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2020-06-15 16:50:43 +0530

    Made responsive and improved the mode colors

--- interactive.js
+++ interactive.js
@@ -1,11 +1,10 @@
 var wmbgcolor = 'rgba(0,0,0,0.1)';
-var dmbgcolor = 'rgba(0,0,0,0.8)';
+var dmbgcolor = 'rgba(0,0,0,0.7)';
+var txtbxbgcolor = 'rgba(243, 231, 212,1)';
+var modebtn = document.getElementById('mode');
+var submitbtn = document.getElementById('submit');
+var body = document.getElementsByTagName('body')[0];
 
-
-
-function Register() {
-    window.open()
-}
 
 function changemode(){
     var tochange = document.getElementById('mode').innerHTML;
@@ -20,20 +19,28 @@ function changemode(){
     }
 }
 
+
 function todarkmode(){
 
-    document.getElementById('mode').innerHTML = 'Light mode';
-    document.getElementsByTagName('body')[0].style.backgroundColor = dmbgcolor;
-    document.getElementById('mode').style.backgroundColor = 'white';
-    document.getElementById('mode').style.color = dmbgcolor;
+    body.style.backgroundColor = dmbgcolor;
+    modebtn.innerHTML = 'Light mode';
+    modebtn.style.backgroundColor = 'white';
+    modebtn.style.color = dmbgcolor;
+    submitbtn.style.backgroundColor = 'rgba(175,238,238,0.6)';
+
 }
 
 function towhitemode(){
 
-    document.getElementById('mode').innerHTML= 'Dark mode';
-    document.getElementsByTagName('body')[0].style.backgroundColor= wmbgcolor;
-    document.getElementById('mode').style.backgroundColor = dmbgcolor;
-    document.getElementById('mode').style.color = 'white';
+    body.style.backgroundColor= wmbgcolor;
+    modebtn.innerHTML= 'Dark mode';
+    modebtn.style.backgroundColor = dmbgcolor;
+    modebtn.style.color = 'white';
+    submitbtn.style.backgroundColor = 'rgba(235, 202, 131,0.5)';
+
 }
 
+//Main starts here
+
 towhitemode();
+
