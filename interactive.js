3a4472bf1e5f4bccdd9117c264ed93b3a745aae7
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2020-06-17 22:16:53 +0530

    Improved CSS hover properties of objects

--- interactive.js
+++ interactive.js
@@ -4,6 +4,7 @@ var txtbxbgcolor = 'rgba(243, 231, 212,1)';
 var modebtn = document.getElementById('mode');
 var submitbtn = document.getElementById('submit');
 var body = document.getElementsByTagName('body')[0];
+var s2 = document.getElementById('onhover');
 
 
 function changemode(){
@@ -27,6 +28,8 @@ function todarkmode(){
     modebtn.style.backgroundColor = 'white';
     modebtn.style.color = dmbgcolor;
     submitbtn.style.backgroundColor = 'rgba(175,238,238,0.6)';
+    //hover properties
+    s2.innerHTML = '#submit:hover{box-shadow: 0 0 8px 1px rgba(175,238,238,1);}#mode:hover{box-shadow: 0 0 8px 1px rgb(255, 255, 255);}'
 
 }
 
@@ -37,6 +40,8 @@ function towhitemode(){
     modebtn.style.backgroundColor = dmbgcolor;
     modebtn.style.color = 'white';
     submitbtn.style.backgroundColor = 'rgba(235, 202, 131,0.5)';
+    //hover properties
+    s2.innerHTML = '#submit:hover{box-shadow: 0 0 8px 1px #fa7f1b;}#mode:hover{box-shadow: 0 0 8px 1px rgb(0, 0, 0);}'
 
 }
 
